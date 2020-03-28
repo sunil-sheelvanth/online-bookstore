d1d9fdc81d517e3df5c3f16a5144e21c10920649
sunil <sunil@LAPTOP-2SQ77GTU.mshome.net>
2020-03-28 21:12:46 +0530

    Created and completed angular feature 1.No no modules which should be downloaded

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,12 @@
+export class Book 
+{
+    sku:string;
+    name:string;
+    description:string;
+    unitprice:number;
+    imageurl:string;
+    active:boolean;
+    unitsInStock:number;
+    createdOn:Date;
+    updatedOn:Date;
+}
---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Book } from 'src/app/common/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+  
+  constructor(private _bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks();
+  }
+
+  listBooks()
+  {
+    this._bookService.getBooks().subscribe(
+      data=>this.books= data
+    )
+  }
+}
